ead46cbf8e369c37990fe1b3ef5b03cef0fb3403
Vladislav Fedoseev <vfedoseev@parallels.com>
2017-12-07 17:03:46 +0300

    added tokens

--- aggregation_service/aggregation_service.py
+++ aggregation_service/aggregation_service.py
@@ -1,11 +1,19 @@
 
 # python modules
-from flask import Flask, jsonify, request, render_template, redirect, url_for
-from flask_cors import CORS
+from flask import Flask, jsonify, request, render_template, redirect, url_for, make_response
+from flask_jwt import JWT, jwt_required, current_identity
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, set_access_cookies,
+    set_refresh_cookies, unset_jwt_cookies
+)
 from requests.exceptions import ReadTimeout
 from threading import Thread
 from tinydb import TinyDB, Query
-import flask_login
+from werkzeug.security import safe_str_cmp
+import base64
+import hashlib
 import json
 import requests
 
@@ -13,15 +21,6 @@ import requests
 import aggregation_lib
 
 application = Flask(__name__)
-CORS(application)
-try:
-    users_db = TinyDB('/Users/amadeus/Documents/rsoi_services/warehouse/users_db.json')
-except:
-    users_db = TinyDB('/srv/www/rsoi_services/warehouse/users_db.json')
-User = Query()
-
-login_manager = flask_login.LoginManager()
-login_manager.init_app(application)
 
 thread = Thread(target = aggregation_lib.reset_billing_total_queue)
 thread.start()
@@ -35,6 +34,168 @@ if application.debug is not True:
     formatter = logging.Formatter("%(asctime)s - %(module)s - %(lineno)d - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     application.logger.addHandler(handler)
+
+
+''' --------------- JWT setup --------------- '''
+application.config['SECRET_KEY'] = 'juf8477*&8sd7nsh.sJ'
+application.config['JWT_TOKEN_LOCATION'] = ['cookies']
+application.config['JWT_COOKIE_CSRF_PROTECT'] = False
+
+clients = [
+    {
+        'client_id': 'orders_service',
+        'redirect_url': 'http://127.0.0.1:8002/connect',
+        'auth_code': 'suydgswe'
+    },
+    {
+        'client_id': 'billing_service',
+        'redirect_url': 'http://127.0.0.1:8003/connect',
+        'auth_code': 'jdusorjg'
+    }
+]
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User('Gilles', 'Gilles', 'c0c4a69b17a7955ac230bfc8db4a123eaa956ccf3c0022e68b8d4e2f5b699d1f'),
+    User('Felix', 'Felix', '72ab994fa2eb426c051ef59cad617750bfe06d7cf6311285ff79c19c32afd236'),
+    User('Paul', 'Paul', '28f0116ef42bf718324946f13d787a1d41274a08335d52ee833d5b577f02a32a'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+jwt = JWTManager(application)
+
+@application.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    username = get_jwt_identity()
+    return jsonify({'token_holder': '{0}'.format(username)}), 200
+''' --------------- --------------- '''
+
+
+''' --------------- Auth and UI methods --------------- '''
+@application.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='username' id='username' placeholder='username'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='Log in'/>
+               </form>
+               '''
+
+    username = request.form['username']
+    password = request.form['password']
+    user = authenticate(username, hashlib.sha256(password.encode('utf-8')).hexdigest())
+    if user:
+        url = 'http://127.0.0.1:8004/token_simple'
+        payload = {'identity': user.id}
+        prms = json.dumps(payload)
+        hdrs = {'Authorization': 'Basic {0}'.format(application.config['SECRET_KEY'])}
+        r = requests.post(url, json = prms, headers = hdrs)
+        print(r.headers)
+        resp = make_response(jsonify({'succ_msg': 'logged in, token set up'}))
+        set_access_cookies(resp, r.headers['Cookie'])
+        return resp, 200
+    else:
+        return jsonify({'err_msg': 'user doesnt exist'}), 400
+
+@application.route('/', methods = ['GET'])
+@jwt_required
+def start():
+    user = get_jwt_identity()
+    return render_template('start.html', prms = {'user': user}), 200
+
+@application.route('/auth', methods = ['GET', 'POST'])
+@jwt_required
+def auth():
+    if request.method == 'GET':
+        return '''
+               <form action='auth' method='POST'>
+                <input type='text' name='client_id' id='client_id' placeholder='client_id'/>
+                <input type='submit' name='Authenticate app'/>
+               </form>
+               '''
+
+    # check client_id
+    client_id = request.form['client_id']
+    client_dict = dict()
+    for client in clients:
+        if client['client_id'] == client_id:
+            print(client)
+            client_dict = client
+            break
+    if not client_dict:
+        return jsonify({'err_msg': 'no such client_id in database'}), 400
+
+    # get client_secret (using auth_code)
+    url = client_dict['redirect_url']
+    payload = {'auth_code': client_dict['auth_code']}
+    prms = json.dumps(payload)
+    r = requests.post(url, json = prms)
+    print(r.headers)
+    client_secret = r.headers['Authorization'].split()[1]
+    print(client_secret)
+
+    # create and store token on session_service
+    user = get_jwt_identity()
+    url = 'http://127.0.0.1:8004/token'
+    payload = {'identity': user, 'client_id': client_dict['client_id']}
+    prms = json.dumps(payload)
+    hdrs = {'Authorization': 'Basic {0}'.format(client_secret)}
+    r = requests.post(url, json = prms, headers = hdrs)
+    resp = r.json()
+
+    return jsonify(resp), 200
+
+def refresh_token(client_id, refresh_token):
+    # check client_id
+    client_dict = dict()
+    for client in clients:
+        if client['client_id'] == client_id:
+            print(client)
+            client_dict = client
+            break
+    if not client_dict:
+        print('no such client_id in database')
+
+    # get client_secret (using auth_code)
+    url = client_dict['redirect_url']
+    payload = {'auth_code': client_dict['auth_code']}
+    prms = json.dumps(payload)
+    r = requests.post(url, json = prms)
+    print(r.headers)
+    client_secret = r.headers['Authorization'].split()[1]
+    print(client_secret)
+
+    # create and store token on session_service
+    url = 'http://127.0.0.1:8004/refresh_token'
+    payload = {'refresh_token': refresh_token, 'client_id': client_dict['client_id']}
+    prms = json.dumps(payload)
+    hdrs = {'Authorization': 'Basic {0}'.format(client_secret)}
+    r = requests.post(url, json = prms, headers = hdrs)
+    print(r.text)
+    print('token refreshed!')
+''' --------------- --------------- '''
 
 
 ''' --------------- Goods methods --------------- '''
@@ -157,14 +318,25 @@ def get_create_order(user_id):
 
 @application.route('/user/<user_id>/orders/<order_id>', methods = ['GET'])
 def order_info(user_id, order_id):
+    # get user tokens
+    url = 'http://127.0.0.1:8004/user/{0}/tokens'.format(str(user_id))
+    r = requests.get(url)
+    user_tokens = r.json()
+    print(user_tokens)
+
     order_dict = {'user': user_id}
 
     # step.1 - get order data
     url = 'http://127.0.0.1:8002/user/{0}/orders/{1}'.format(user_id, order_id)
     prms = {}
-    hdrs = {'accept': 'application/json'}
+    hdrs = {'Authorization': 'JWT {0}'.format(user_tokens.get('orders_service', 'invalid_token'))}
     try:
         r = requests.get(url, params = prms, headers = hdrs, timeout = 10)
+        if r.status_code == 401:
+            r_dict = r.json()
+            if r_dict['description'] == 'Signature has expired':
+                refresh_token('orders_service', user_tokens.get('orders_service_refresh', 'invalid_token'))
+            return jsonify(r.json()), 401
     except (OSError, ReadTimeout) as err:
         return jsonify({'err_msg': 'orders service unavailable...'}), 503
     user_order = r.json()
@@ -274,10 +446,6 @@ def perform_billing(user_id, order_id):
 
 
 ''' --------------- General methods --------------- '''
-@application.route('/', methods = ['GET'])
-def start():
-    return render_template('start.html')
-
 @application.errorhandler(404)
 def page_not_found(e):
     return jsonify({'err_msg': 'Page not found'}), 404
--- orders_service/orders_service.py
+++ orders_service/orders_service.py
@@ -1,6 +1,8 @@
 
 # python modules
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
 import json
 
 application = Flask(__name__)
@@ -19,6 +21,59 @@ if application.debug is not True:
     application.logger.addHandler(handler)
 
 
+''' --------------- JWT Oauth2 setup --------------- '''
+application.config['SECRET_KEY'] = 'hdba763_sjahd&^bdgHsS'
+authorization_code = 'suydgswe'
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User('Gilles', 'Gilles', 'c0c4a69b17a7955ac230bfc8db4a123eaa956ccf3c0022e68b8d4e2f5b699d1f'),
+    User('Felix', 'Felix', '72ab994fa2eb426c051ef59cad617750bfe06d7cf6311285ff79c19c32afd236'),
+    User('Paul', 'Paul', '28f0116ef42bf718324946f13d787a1d41274a08335d52ee833d5b577f02a32a'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+jwt = JWT(application, authenticate, identity)
+
+@application.route('/protected')
+@jwt_required()
+def protected():
+    return jsonify({'token_holder': current_identity.id}), 200
+''' --------------- --------------- '''
+
+
+@application.route('/connect', methods = ['POST'])
+def connect():
+    connect_json = request.get_json(force=True)
+    connect_dict = json.loads(connect_json)
+    auth_code = connect_dict['auth_code']
+    if not (auth_code == authorization_code):
+        return jsonify({'err_msg': 'auth_code incorrect'}), 400
+
+    resp = make_response(jsonify({'succ_msg': 'can connect!'}))
+    resp.headers['Authorization'] = 'Basic {0}'.format(application.config['SECRET_KEY'])
+    return resp, 200
+
+
 @application.route('/user/<user_id>/orders', methods = ['GET', 'POST'])
 def get_create_orders(user_id):
     if request.method == 'GET': # get orders info
@@ -33,6 +88,7 @@ def get_create_orders(user_id):
         return jsonify({'order_id': order_id})
 
 @application.route('/user/<user_id>/orders/<order_id>', methods = ['GET'])
+@jwt_required()
 def order_info(user_id, order_id):
     user_order = orders_lib.get_order_info(int(order_id), user_id)
     return jsonify(user_order)
